2ab78c54c3d4c9f6f2d703e3713b185c97b9ab01
mn <eaglel4rk@gmail.com>
2025-02-28 13:22:22 +0900

    00入力に対応

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 let num_1 = "", num_2 = "", result;
 let operator = "";
 
-/*
-    - numを入力後"00"が入力できない
-    - .を実装する
-*/
-
 $(".num").click(function() { 
     let input = $(this).text();
 
@@ -13,26 +8,15 @@ $(".num").click(function() {
     if(num_1 == "0" && input == "0") return;
     if(num_2 == "0" && input == "0") return;
 
-    // 先頭に00が入らないようにする
-    if(!num_1 && input === "00") return;
-    if(!num_2 && input === "00") return;
-
-    if (input === ".") {
-        if (!operator) {
-            if(num_1.includes(".")) return;
-            if(num_1 === "") return;
-        } else {
-            if(num_2.includes(".")) return;
-            if(!num_2) return;
-        }
-    }
-
+    //計算後.や0が入力されたらリセット
     if(num_1 == result && !operator){
         if(input !== "." || num_1 !== "0."){
             num_1 = "";
             $(".display").text("");
         }
     }
+
+    //num_1が0の時数字を入力されたら上書きする。（.を除く）
     if(num_1 == "0" && !operator){
         if(input !== "."){
             num_1 = "";
@@ -40,7 +24,15 @@ $(".num").click(function() {
         }
     }
 
-    if(!operator ? num_1 += input : num_2 += input);
+    // 計算
+    if(!operator){
+        if(!num_1 && input === "00") return;
+        num_1 += input;
+    }else{
+        if(!num_2 && input === "00") return;
+        num_2 += input;
+    }
+
     $(".display").text(num_1 + operator + num_2);
 });
 
